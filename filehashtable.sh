e6e90f5bba3f8c383f13c36d8a4aff6e9bd2b0ba
Amiel Summers <amiel@wheezy.(none)>
2013-11-19 04:40:23 -0500

    Cosmetic changes.

--- filehashtable.sh
+++ filehashtable.sh
@@ -7,7 +7,8 @@
 # Depends: rhash, bash
 # Recommends: gedit, sendmail, cron
 
-# Copyright (C) 2013 Amiel Summers
+# Copyright (C) 2013 Amiel Summers,  amiel@amieltech.com
+#
 #        This program is free software; you can redistribute it and/or
 #        modify it under the terms of the GNU General Public License
 #        as published by the Free Software Foundation; either version 2
